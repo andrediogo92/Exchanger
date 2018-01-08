b4aa7244a52fbf5adee58744bb4d6e7e637f14b9
Seriyin <redan.blue27@gmail.com>
2018-01-08 00:35:07 +0000

    Added a definitions header file.

---
+++ main.hrl
@@ -0,0 +1,13 @@
+-define(PORT_EXCHANGE,10000).
+-define(PORT_CLIENTS,10001).
+-define(NUM_RETRIES,5).
+-define(EXCHANGER_NAME,exchanger).
+-define(CLIENTER_NAME, clienter).
+-define(LOGINER_NAME,loginer).
+-define(KNOWN, #{<<"Adolph">> => {<<"1111">>, false}, 
+                 <<"Bert">> => {<<"2222">>, false},
+                 <<"Carlos">> => {<<"3333">>, false},
+                 <<"Diego">> => {<<"4444">>, false},
+                 <<"Earl">> => {<<"5555">>, false},
+                 <<"Frederick">> => {<<"6666">>, false},
+                 <<"Gonzo">> => {<<"7777">>, false}}).
