54fbd47eff0632aff1d2194a5b96db4bdfc4bf7a
Seriyin <redan.blue27@gmail.com>
2018-01-23 20:57:19 +0000

    Refactored around using wrapper to pattern match on decode.

--- src/user.erl
+++ src/user.erl
@@ -23,61 +23,25 @@ start_with_link(Ident, Multipart) ->
 
 
 %% @doc
-%% Tries to check if a message can be decoded as a {@link login_pb:login_request. login request}.
-%%
-%% Decoding failure results in error.
+%% Tries to check if a user can login.
 %% @end
 %% @param Multipart is the message to try to decode.
-%% @return A tuple with error or the result of {@link login_manager:login, attempting to login in the login_manager}.
-check_valid_login(Multipart) ->
-    try login:decode_msg(Multipart,'login_request') of
-        Login ->
-            #{user := User, password := Password} = Login,
-            {login_manager:login(User,Password),{User,Password}}
-    catch    
-        error:Err ->
-            {error, Err}
+%% @return A tuple with error or a pair {User, Password} result of succesful {@link login_manager:login.
+check_valid_login(Ident, Multipart) ->
+    #{user := User, password := Password} = Multipart,
+    case login_manager:login(User,Password) of
+        {_, ok} ->
+            ?CLIENTER_NAME ! {login, User, Ident},
+            {User, Password};
+        Err ->
+            Err
     end.
 
+just_logout(Multipart) ->
+    #{user := User} = Multipart,
+    login_manager:logout(User),
+    logout.
 
-check_trade_request(Ident, {Logged, _}, Multipart) ->
-    try trade_pb:decode_msg(Multipart,'trade_order') of
-        Trade ->
-            #{buy_or_sell := Order,
-              user := User} = Trade,
-            case Logged=:=User of
-                true ->
-                    case Order of
-                        true -> 
-                            {trade_buy, Ident, Trade};
-                        false ->
-                            {trade_sell, Ident, Trade}
-                    end;
-                false ->
-                    login_manager:logout(Logged),
-                    {logout,Logged}
-            end
-    catch
-        error:Err ->
-            {error, Err}
-    end.
-
-
-check_address_request(Multipart) ->
-    try trade_pb:decode_msg(Multipart,'address_request') of
-        Address ->
-            #{name := Name,
-              type := Type} = Address,
-            case Type of
-                'DIRECTORY' ->
-                    dir_address;
-                'EXCHANGE' ->
-                    {ex_address, Name}
-            end
-    catch
-        error:Err ->
-            {error, Err}
-    end.
             
 
 
@@ -91,17 +55,17 @@ check_address_request(Multipart) ->
 %% @param Ident is the ZeroMQ identity so client demuxer knows who to send messages to.
 %% @param Multipart is the message that may contain a {@link login_pb:login_request. login request}.
 handle_login(Ident, Multipart) ->
-    case check_valid_login(Multipart) of
-        {ok, Login} -> 
-            ?CLIENTER_NAME ! {login, Ident},
+    case handle_message(Ident, Multipart) of
+        Login={User, _} -> 
+            ?CLIENTER_NAME ! {login, User, Ident},
             handle_requests(Ident, Login, #{});
         M ->
             {error, M}
     end.
 
 handle_login(Ident, Multipart, first) ->
-    case check_valid_login(Multipart) of
-        {ok, Login={User,_}} -> 
+    case handle_message(Ident, Multipart) of
+        Login={User, _} -> 
             ?CLIENTER_NAME ! {login, User, Ident},
             case handle_requests(Ident, Login, #{}) of
                 logout ->
@@ -120,16 +84,18 @@ handle_login(Ident, Multipart, first) ->
 %% @end
 handle_requests(Ident, Login={User,_}, Exchanges) ->
     receive
-        {logout, User} ->
-            ?CLIENTER_NAME ! {logout, User},
-            logout;
         {exchange, Name, PID} ->
             handle_requests(Ident, 
                             Login, 
                             map:put(Name, PID, Exchanges));
         Multipart ->
-            handle_multipart(Ident, Login, Multipart, Exchanges),
-            handle_requests(Ident, Login, Exchanges)
+            case handle_multipart(Ident, Login, Multipart, Exchanges) of
+                logout ->
+                    ?CLIENTER_NAME ! {logout, User},
+                    logout;
+                _ ->
+                    handle_requests(Ident, Login, Exchanges)
+            end
     after ?STUPID_TIME_OUT ->
         ?CLIENTER_NAME ! {logout, User},
         ?CLIENTER_NAME ! {exit, Ident}
@@ -148,53 +114,62 @@ handle_requests(Ident) ->
     end.
 
 
-handle_multipart(Ident, Login, Multipart, Exchanges) ->
-    case handle_message(Ident, Login, Multipart) of
-        {error, _} ->
-            error;
+handle_multipart(Ident, {User,_}, Multipart, Exchanges) ->
+    case handle_message(Ident, Multipart) of
+        logout ->
+            ?CLIENTER_NAME ! {logout, User},
+            logout;
         {ex_address, Ident, Name} ->
             case map:find(Name, Exchanges) of
                 {ok, PID} ->
                     PID ! {ex_address, Ident, Name};
                 error ->
                     ?EXCHANGER_NAME ! {ex_address, Ident, Name}
-            end;
+            end,
+            nothing;
         {dir_address, Ident, M} ->
-            ?CLIENTER_NAME ! {dir_address, Ident, M};
-        {T, Ident, Trade} ->
-            #{exchange := Exchange}=Trade,
+            ?CLIENTER_NAME ! {dir_address, Ident, M},
+            nothing;
+        M = {trade, Ident, Exchange, _} ->
             case map:find(Exchange, Exchanges) of
                 {ok, PID} ->
-                    PID ! {T, Ident, Trade};
+                    PID ! M;
                 error ->
-                    ?EXCHANGER_NAME ! {T, Ident, Trade}
-            end;
-        M ->
-            ?CLIENTER_NAME ! M
+                    ?EXCHANGER_NAME ! M
+            end,
+            nothing;
+        _ ->
+            error
     end.
 
 
 
-handle_message(Ident, Login, Multipart) ->
-    case check_trade_request(Ident, Login, Multipart) of
-        L={_, _, _} ->
-            L;
-        {error, _} ->
-            handle_address_request(Ident, Multipart)
+handle_message(Ident, Multipart) ->
+    case wrapper_client_pb:decode_msg(Multipart, 'WrapperMessageClient') of
+        #{isLogin := true, message := M} ->
+            check_valid_login(Ident, M);
+        #{isLogout := true, message := M} ->
+            just_logout(M);
+        #{isAddressRequest := true, message := M} ->
+            handle_address_request(Ident, M);
+        #{isTrade := true, message := #{exchange := Exchange}} ->
+            {trade, Ident, Exchange, Multipart}
     end.
         
-handle_address_request(Ident, Multipart) ->
-    case check_address_request(Multipart) of
-        dir_address ->
-            {Host, Port} = ?DIRECTORY,
-            Adrs = #{host => Host, 
-                     port => Port, 
-                     name => <<"index.html">>, 
-                     type => 'DIRECTORY'},
-            M = addresses_pb:encode_msg(Adrs, 'address'),
-            {dir_address, Ident, M};
-        {ex_address, Name} ->
-            {ex_address, Ident, Name};
-        {error, M} ->
-            {error, M}
+
+handle_address_request(Ident, Address) ->
+    #{name := Name,
+      type := Type} = Address,
+    case Type of
+        'DIRECTORY' ->
+                {Host, Port} = ?DIRECTORY,
+                Adrs = #{host => Host, 
+                         port => Port, 
+                         name => <<"index.html">>, 
+                         type => 'DIRECTORY'},               
+                New = #{isAddress => true, message => Adrs},
+                M = wrapper_client_pb:encode_msg(New),
+                {dir_address, Ident, M};
+        'EXCHANGE' ->
+            {ex_address, Ident, Name}
     end.
